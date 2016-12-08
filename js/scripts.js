2dd78b002b6c3e1f0d49e8af445691a1d17b5847
danilocontini <contato@danilocontini.com.br>
2016-12-08 18:19:52 -0200

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        
+        event.preventDefault();
+    });
+    
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function () {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    });
+    $('#mainNav').on('gbs.affix', function(){
+	    $('.logo').attr('src','img/yubuy.svg');
+    });
+    $('#mainNav').on('affix-top.bs.affix', function(){
+	    $('.logo').attr('src','img/yubuyWhite.svg');
+    });
+
+}(jQuery)); // End of use strict
